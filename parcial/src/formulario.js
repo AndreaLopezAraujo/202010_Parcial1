cc9162c40d3edea3408a014a83a9eb5b9ccd1183
Andrea Lopez <ac.lopez@uniandes.edu.co>
2020-03-09 08:35:29 -0500

    a

---
+++ parcial/src/formulario.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+export default class formulario extends Component {
+  render() {
+    return (
+      <div>
+        <form>
+          <div class="form-group">
+          <label for="exampleInputEmail1">NIT</label>
+          <input  class="form-control" id="exampleInputEmail1"  placeholder="Número de Identificacíon Tributaria"></input>
+          </div>
+          <div class="form-group">
+          <label for="exampleInputEmail1">Razón Social</label>
+          <input  class="form-control" id="exampleInputEmail1"  placeholder="Nombre del participante"></input>
+          </div>
+          <button type="submit" class="btn btn-primary">Registrarse</button>
+        </form>
+      </div>
+    );
+  }
+}
---
+++ parcial/src/ofertas.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ofertas extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
