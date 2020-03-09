ff3003b2e57b179c8a7c5233726ce891951abcc2
Andrea Lopez <ac.lopez@uniandes.edu.co>
2020-03-09 09:55:38 -0500

    limpieza 2

--- parcial/src/index.js
+++ parcial/src/index.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import Formulario from './formulario'
 import Ofertas from './ofertas'
-import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import $ from 'jquery';
 import Popper from 'popper.js';
@@ -14,7 +11,3 @@ import 'bootstrap/dist/js/bootstrap.bundle.min';
 ReactDOM.render(<Formulario />, document.getElementById('root'));
 ReactDOM.render(<Ofertas />, document.getElementById('ofertas'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
