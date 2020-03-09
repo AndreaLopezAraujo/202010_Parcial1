f1f2e6bbbee4549fc128a377ba2b76bd99301782
Andrea Lopez <ac.lopez@uniandes.edu.co>
2020-03-09 11:12:58 -0500

    Update formulario.js

--- parcial/src/componets/formulario.js
+++ parcial/src/componets/formulario.js
@@ -3,17 +3,38 @@ import {usuarios} from './Json/usuarios.json'
 console.log(usuarios)
 
 export default class formulario extends Component {
+  constructor()
+  {
+    super();
+    this.state={
+      NIT:'',
+      Razon:''
+    };
+    this.handleImput=this.handleImput.bind(this);
+
+  }
+  handleImput(e)
+  {
+    const {value,id}=e.target;
+    this.setState(
+      {
+        [id]:value
+      }
+    )
+    console.log("aqui");
+    console.log(this.state);
+  };
   render() {
     return (
       <div>
         <form>
           <div class="form-group">
           <label for="exampleInputEmail1">NIT</label>
-          <input  class="form-control" id="exampleInputEmail1"  placeholder="Número de Identificacíon Tributaria"></input>
+          <input  class="form-control" id="NIT"  placeholder="Número de Identificacíon Tributaria" onChange={this.handleImput}></input>
           </div>
           <div class="form-group">
           <label for="exampleInputEmail1">Razón Social</label>
-          <input  class="form-control" id="exampleInputEmail1"  placeholder="Nombre del participante"></input>
+          <input  class="form-control" id="Razon"  placeholder="Nombre del participante" onChange={this.handleImput}></input>
           </div>
           <button type="submit" class="btn btn-primary">Registrarse</button>
         </form>
