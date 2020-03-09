371125ebd9c41f3a3f8902c76155dd878f0364f2
Andrea Lopez <ac.lopez@uniandes.edu.co>
2020-03-09 10:04:44 -0500

    arr

--- parcial/src/index.js
+++ parcial/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Formulario from './formulario'
-import Ofertas from './ofertas'
+import Formulario from './componets/formulario.js'
+import Ofertas from './componets/ofertas.js'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import $ from 'jquery';
 import Popper from 'popper.js';
