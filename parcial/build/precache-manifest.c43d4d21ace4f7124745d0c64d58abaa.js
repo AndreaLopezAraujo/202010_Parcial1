9db796a58ba8a5c1cee8dde4219add9e642d2ea4
Andrea Lopez <ac.lopez@uniandes.edu.co>
2020-03-09 08:21:59 -0500

    a

---
+++ parcial/build/precache-manifest.c43d4d21ace4f7124745d0c64d58abaa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "447e130711efffa5a58892b097a1c11f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e382c2884822d5785d4",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b196260380542256a60e",
+    "url": "/static/js/2.25f0f476.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.25f0f476.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e382c2884822d5785d4",
+    "url": "/static/js/main.639950b1.chunk.js"
+  },
+  {
+    "revision": "ac92cdfb6238f1735c98",
+    "url": "/static/js/runtime-main.b4c3dbab.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
